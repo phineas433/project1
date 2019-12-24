950725b088e5f2e838f073a3241e410845e37d56
phineas <phineas@10.232.204.216>
2019-12-24 22:46:38 +0800

    12/24
    Signed-off-by: phineas <phineas@10.232.204.216>

--- JS/SeniorEntertainment/src/WebPage.java
+++ JS/SeniorEntertainment/src/WebPage.java
@@ -4,34 +4,30 @@ import java.util.ArrayList;
 public class WebPage {
 	public String url;
 	public String name;
-	public KeywordCounter counter;
 	public double score;
 
 	public WebPage(String url, String name) {
 		this.url = url;
 		this.name = name;
-		this.counter = new KeywordCounter(url);
 	}
 
 	public void setScore(ArrayList<Keyword> keywords) {
 		this.score = 0;
 
-		try {
+
 			for (Keyword k : keywords) {
 				if (url.length() > 50) {
-					this.score += counter.countKeyword(k.name) * k.weight * 8;
+					//this.score += counter.countKeyword(k.name) * k.weight * 8;
 					if (url.contains("tour guide")) {
 						this.score *= 0.5;
 					} else if (url.contains("senior")) {
 						this.score *= 0.8;
 					}
 				} else {
-					this.score += counter.countKeyword(k.name) * k.weight;
+					//this.score += counter.countKeyword(k.name) * k.weight;
 				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 	@Override
